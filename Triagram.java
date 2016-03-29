e44338e7ad29e9a6c0152e75e15bce89b85bcad4
Elton Chan <eltonchan@cal.berkeley.edu>
2016-03-29 01:10:21 -0700

    Implement basic IO

--- Triagram.java
+++ Triagram.java
@@ -2,7 +2,20 @@ import java.io.*;
 import java.util.*;
 
 public class Triagram {
-  public static void main (String[] args) {
-    System.out.println("hello world");
+  public static void main (String[] args) throws IOException {
+    BufferedReader inputStream = null;
+
+    try {
+      inputStream = new BufferedReader(new FileReader("sample_text.txt"));
+
+      String line;
+      while ((line = inputStream.readLine()) != null) {
+        System.out.println(line);
+      }
+    } finally {
+      if (inputStream != null) {
+        inputStream.close();
+      }
+    }
   } // end main method
 } // end class
