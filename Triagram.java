54649584504f545122408e8c476899693325c248
Elton Chan <eltonchan@cal.berkeley.edu>
2016-03-29 00:46:42 -0700

    Add basic setup

---
+++ Triagram.java
@@ -0,0 +1,8 @@
+import java.io.*;
+import java.util.*;
+
+public class Triagram {
+  public static void main (String[] args) {
+    System.out.println("hello world");
+  } // end main method
+} // end class
--- main.java
+++
@@ -1,5 +0,0 @@
-public class MyFirstApp {
-  public static void main (String[] args) {
-
-  } // end main method
-} // end class
